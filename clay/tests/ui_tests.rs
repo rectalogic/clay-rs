3213aacbe56a8045612c98fd682273e050a2184e
Andrew Wason <rectalogic@rectalogic.com>
2024-12-31 15:24:57 -0500

    drop clay! macro for callback function. Still avoid allocations though.

--- clay/tests/ui_tests.rs
+++ clay/tests/ui_tests.rs
@@ -16,7 +16,7 @@ fn test_simple_ui() {
     let dimensions = clay::Dimensions::new(300.0, 300.0);
     arena.initialize(dimensions, clay::default());
     let mut arena = arena;
-    let render_commands = arena.render(|| {
+    let render_commands = arena.render(|builder| {
         let color = clay::Color {
             r: 240.,
             g: 189.,
@@ -25,49 +25,69 @@ fn test_simple_ui() {
         };
         const FONT_ID_BODY_24: u16 = 2;
 
-        clay::clay!((
-            clay::IdI(clay::String::from("HeroBlob"), 1),
-            clay::Layout {
-                sizing: clay::Sizing {
-                    width: clay::SizingAxis::grow(0.0, 480.0),
-                    ..clay::default()
-                },
-                padding: clay::Padding { x: 16, y: 16 },
-                child_gap: 16,
-                child_alignment: clay::ChildAlignment {
-                    y: clay::LayoutAlignmentY::Center,
-                    ..clay::default()
-                },
-                ..clay::default()
-            },
-            clay::Border::outside_radius(2, color, 10.0)
-        ) {
-            clay::clay!(
-                (
-                    clay::Id(clay::String::from("CheckImage")),
-                    clay::Layout {
-                        sizing: clay::Sizing { width: clay::SizingAxis::fixed(32.), ..clay::default() },
+        builder.build(
+            &[
+                clay::IdI(clay::String::from("HeroBlob"), 1).into(),
+                clay::Layout {
+                    sizing: clay::Sizing {
+                        width: clay::SizingAxis::grow(0.0, 480.0),
                         ..clay::default()
                     },
-                    clay::Image { source_dimensions: clay::Dimensions { width: 128., height: 128.}, ..clay::default() } // XXX need extended sourceUrl
-                ) {
+                    padding: clay::Padding { x: 16, y: 16 },
+                    child_gap: 16,
+                    child_alignment: clay::ChildAlignment {
+                        y: clay::LayoutAlignmentY::Center,
+                        ..clay::default()
+                    },
+                    ..clay::default()
                 }
-            );
-            clay::Text {
-                font_size: 18,
-                font_id: FONT_ID_BODY_24,
-                text_color: color,
-                ..clay::default()
-            }.with(clay::String::from("Some text here"));
-        });
+                .into(),
+                clay::Border::outside_radius(2, color, 10.0).into(),
+            ],
+            |builder| {
+                builder.build(
+                    &[
+                        clay::Id(clay::String::from("CheckImage")).into(),
+                        clay::Layout {
+                            sizing: clay::Sizing {
+                                width: clay::SizingAxis::fixed(32.),
+                                ..clay::default()
+                            },
+                            ..clay::default()
+                        }
+                        .into(),
+                        clay::Image {
+                            source_dimensions: clay::Dimensions {
+                                width: 128.,
+                                height: 128.,
+                            },
+                            ..clay::default()
+                        }
+                        .into(), // XXX need extended sourceUrl
+                    ],
+                    |_| {},
+                );
+                builder.build(
+                    &[clay::Text {
+                        font_size: 18,
+                        font_id: FONT_ID_BODY_24,
+                        text_color: color,
+                        ..clay::default()
+                    }
+                    .with(clay::String::from("Some text here"))],
+                    |_| {},
+                );
+            },
+        );
         // CLAY(CLAY_IDI("HeroBlob", index), CLAY_LAYOUT({ .sizing = { CLAY_SIZING_GROW({ .max = 480 }) }, .padding = {16, 16}, .childGap = 16, .childAlignment = {.y = CLAY_ALIGN_Y_CENTER} }), CLAY_BORDER_OUTSIDE_RADIUS(2, color, 10)) {
         //     CLAY(CLAY_IDI("CheckImage", index), CLAY_LAYOUT({ .sizing = { CLAY_SIZING_FIXED(32) } }), CLAY_IMAGE({ .sourceDimensions = { 128, 128 }, .sourceURL = imageURL })) {}
         //     CLAY_TEXT(text, CLAY_TEXT_CONFIG({ .fontSize = fontSize, .fontId = FONT_ID_BODY_24, .textColor = color }));
         // }
     });
+    // Internally generated id's can change, not a good test...
     let expected = [
         r#"RenderCommand { bounding_box: BoundingBox { x: 16.0, y: 16.0, width: 32.0, height: 32.0 }, config: Image { image_data: 0x0, source_dimensions: Dimensions { width: 128.0, height: 128.0 } }, text: String { chars: "" }, id: 1782946882, command_type: Image }"#,
-        r#"RenderCommand { bounding_box: BoundingBox { x: 64.0, y: 23.0, width: 140.0, height: 18.0 }, config: Text { text_color: Color { r: 240.0, g: 189.0, b: 100.0, a: 255.0 }, font_id: 2, font_size: 18, letter_spacing: 0, line_height: 0, wrap_mode: ClayTextWrapWords }, text: String { chars: "Some text here" }, id: 787525995, command_type: Text }"#,
+        r#"RenderCommand { bounding_box: BoundingBox { x: 64.0, y: 23.0, width: 140.0, height: 18.0 }, config: Text { text_color: Color { r: 240.0, g: 189.0, b: 100.0, a: 255.0 }, font_id: 2, font_size: 18, letter_spacing: 0, line_height: 0, wrap_mode: ClayTextWrapWords }, text: String { chars: "Some text here" }, id: 78651382, command_type: Text }"#,
         r#"RenderCommand { bounding_box: BoundingBox { x: 0.0, y: 0.0, width: 300.0, height: 64.0 }, config: Border { left: BorderStyle { width: 2, color: Color { r: 240.0, g: 189.0, b: 100.0, a: 255.0 } }, right: BorderStyle { width: 2, color: Color { r: 240.0, g: 189.0, b: 100.0, a: 255.0 } }, top: BorderStyle { width: 2, color: Color { r: 240.0, g: 189.0, b: 100.0, a: 255.0 } }, bottom: BorderStyle { width: 2, color: Color { r: 240.0, g: 189.0, b: 100.0, a: 255.0 } }, between_children: BorderStyle { width: 0, color: Color { r: 0.0, g: 0.0, b: 0.0, a: 0.0 } }, corner_radius: CornerRadius { top_left: 10.0, top_right: 10.0, bottom_left: 10.0, bottom_right: 10.0 } }, text: String { chars: "" }, id: 2979443697, command_type: Border }"#,
     ];
     for (expected_command, command) in zip(expected, render_commands) {
